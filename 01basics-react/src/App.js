38b646c106a800302618d6a72dd909b32e4a72ff
Rahul Naskar <rahul9875458880@gmail.com>
2024-06-30 01:40:14 +0530

    create two react project by reaxt app and by vite

---
+++ 01basics-react/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <h1>React Learning</h1>
+      <h1>hello world</h1>
+      <div>
+        <p>I am learning react</p>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
