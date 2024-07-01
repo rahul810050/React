79825cf7ed163bf9e59b6fd6c60005c66790322d
Rahul Naskar <rahul9875458880@gmail.com>
2024-07-01 23:47:19 +0530

    02 user Dash Board project

---
+++ React-Projects/02UseofUseEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import UserDashboard from './UserDashboard';
+
+function App(){
+  return(
+    <>
+    <UserDashboard/>
+    </>
+  )
+}
+
+export default App
