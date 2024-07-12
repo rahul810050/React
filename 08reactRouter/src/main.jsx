fd91acbc114db70899788ccbb6b597f7f0db6765
Rahul Naskar <rahul9875458880@gmail.com>
2024-07-12 21:23:37 +0530

    context api project

--- 08reactRouter/src/main.jsx
+++ 08reactRouter/src/main.jsx
@@ -9,6 +9,7 @@ import Home from './Components/Home/Home.jsx'
 import About from './Components/About/About.jsx'
 import Contact from './Components/ContactUs/ContactUs.jsx'
 import User from './Components/User/User.jsx'
+// import Github from './Components/Github/Github.jsx'
 import Github, { githubInfoLoader } from './Components/Github/Github.jsx'
 
 
